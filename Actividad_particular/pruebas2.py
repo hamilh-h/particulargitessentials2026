29d6210519f5953d6a47121bbb42168cef9c6762
Hamil <hasihamil@gmail.com>
2026-05-21 11:32:44 -0300

    Agregando archivo para realizar la actividad

---
+++ Actividad_particular/pruebas2.py
@@ -0,0 +1,17 @@
+#7. Crear un vector de 18 valores aleatorios enteros entre 1 y 50, y mostrar por pantalla el promedio de todos sus valores
+
+import random
+
+vector = []
+for i in range(18):
+    vector.append(random.randint(1, 50))
+
+print("Vector original:")
+print(vector)
+
+suma = 0
+for i in range(len(vector)):
+    suma += vector[i]
+
+promedio = suma / len(vector)
+print("El promedio de todos los valores es:", promedio)
