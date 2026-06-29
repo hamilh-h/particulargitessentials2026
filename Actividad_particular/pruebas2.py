1bc353c85dcc8efcb2d7700147315a12d9d78bdf
Pao <89752372+paobtorres@users.noreply.github.com>
2026-06-29 18:44:02 -0300

    Fix variable name for mean calculation

--- Actividad_particular/pruebas2.py
+++ Actividad_particular/pruebas2.py
@@ -13,5 +13,5 @@ suma = 0
 for i in range(len(vector)):
     suma += vector[i]
 
-mean_value = suma / len(vector)
-print("El promedio de todos los valores es:", mean_value)
+promedio = suma / len(vector)
+print("El promedio de todos los valores es:", promedio)
